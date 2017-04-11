8b49b92dae0a69b236b742810899a71323394080
funmi_a <funmiadewodu@yahoo.com>
2017-04-11 23:42:29 +0100

    Adding exercise 2 to 10

---
+++ ex5.rb
@@ -0,0 +1,21 @@
+name = "Zed A. Shaw"
+age = 35
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_in_centimetres = height * 2.54
+weight_in_kilos = weight * 0.453592
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
+ puts "My height in centimetres is #{height_in_centimetres}cm and my weight in kilos
+ is #{weight_in_kilos}kg."
